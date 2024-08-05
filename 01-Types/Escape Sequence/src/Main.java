5276149e3bc7c3118d2d2c67ba7f35ee6cec70f2
Rahul Nainala <rahulnainala2@gmail.com>
2024-08-05 22:27:29 +0530

    Added files - 02

--- 01-Types/Escape Sequence/src/Main.java
+++ 01-Types/Escape Sequence/src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Escape Sequences (/)
+        //Below are the mostly used ones
+        //To include "" in the output
+        System.out.println("Hello \"world!\"");
+
+        //To print a new line
+        System.out.println("Hello \n The World");
+
+        //To print a backslash in the output (\)
+        System.out.println("C:\\windows\\cmd\\hello");
+
+        //To print a Tab space
+        System.out.println("Hello \t World");
     }
 }
