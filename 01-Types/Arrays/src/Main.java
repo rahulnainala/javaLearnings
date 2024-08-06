210490bed4f95dabb10b267e4d92415213e891ea
Rahul Nainala <rahulnainala2@gmail.com>
2024-08-06 23:10:17 +0530

    Added files -03

--- 01-Types/Arrays/src/Main.java
+++ 01-Types/Arrays/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+       /* Declaration of an Array
+        * int[] variable_name = new int[sizeof_Array];
+        */
+        int[] number = new int[10];
+        
         System.out.println("Hello world!");
+
     }
 }
